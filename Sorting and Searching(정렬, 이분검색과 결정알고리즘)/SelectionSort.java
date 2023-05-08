2b09b93d9e318b0b2203e70f8b0210c2d0c40a3c
Yewon Kim <61744872+yeahxne@users.noreply.github.com>
2023-05-08 23:52:03 +0900

    Update SelectionSort.java

--- Sorting and Searching(정렬, 이분검색과 결정알고리즘)/SelectionSort.java
+++ Sorting and Searching(정렬, 이분검색과 결정알고리즘)/SelectionSort.java
@@ -6,11 +6,15 @@ public class SelectionSort {
     public int[] solution(int N, int[] nArray) {
         for(int i=0; i<N-1; i++) {
             int idx = i;
+            
+            /* 최소값을 갖고있는 인덱스 찾기 */
             for(int j=i+1; j<N; j++) {
                 if(nArray[j]<nArray[idx]) {
                     idx = j;
                 }
             }
+            
+            /* i번째 값과 찾은 최소값을 서로 교환 */
             int tmp = nArray[i];
             nArray[i] = nArray[idx];
             nArray[idx] = tmp;
