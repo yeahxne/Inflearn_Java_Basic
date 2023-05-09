870253a4e5e477da86d03d29f7f01083a2811c2e
yeahxne <dpdnjs0125@naver.com>
2023-05-09 20:47:20 +0900

    Revert "Merge branch 'main' of https://github.com/yeahxne/Inflearn_Java_Basic"
    This reverts commit d8a7d5cc90c501d11128301f2208ad18df87904f, reversing
    changes made to 2e46b9ed528dc3ff73ce1cc7bc3e5c8698a37855.

--- Sorting and Searching(정렬, 이분검색과 결정알고리즘)/SelectionSort.java
+++ Sorting and Searching(정렬, 이분검색과 결정알고리즘)/SelectionSort.java
@@ -6,16 +6,11 @@ public class SelectionSort {
     public int[] solution(int N, int[] nArray) {
         for(int i=0; i<N-1; i++) {
             int idx = i;
-            
-            /* 최소값을 갖고있는 인덱스 찾기 */
             for(int j=i+1; j<N; j++) {
                 if(nArray[j]<nArray[idx]) {
                     idx = j;
                 }
             }
-            
-            /* i번째 값과 찾은 최소값을 서로 교환 */
-            /* Java에서는 swap() 함수가 없기 때문에, 직접 구현해주어야 한다. */
             int tmp = nArray[i];
             nArray[i] = nArray[idx];
             nArray[idx] = tmp;
